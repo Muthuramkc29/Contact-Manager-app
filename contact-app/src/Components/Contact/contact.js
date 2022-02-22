94541dd69f83da1338b566698f7c2e46c6c49709
Muthuram Kc <you@example.com>
2022-02-22 17:05:44 +0530

    View Icon Added with functionalities

--- contact-app/src/Components/Contact/contact.js
+++ contact-app/src/Components/Contact/contact.js
@@ -13,25 +13,31 @@ const Contact = ({ contacts, removeContactHandler }) => {
         contacts.map((contact) => (
           <div key={contact.id}>
             <div className="my-3 d-flex justify-content-between align-items-center border border-dark p-2">
-              <Link
-                style={{ color: "black", textDecoration: "none" }}
-                to={`/${contact.id}`}
-              >
-                <div className="d-flex justify-content-between align-items-center gap-2">
-                  <div>
-                    <img
-                      src={userIcon}
-                      alt="User-Icon"
-                      style={{ width: 43, height: 43 }}
-                    />
-                  </div>
-                  <div>
-                    <h5 className="name">{contact.name}</h5>
-                    <p className="email">{contact.email}</p>
-                  </div>
+              <div className="d-flex justify-content-between align-items-center gap-2">
+                <div>
+                  <img
+                    src={userIcon}
+                    alt="User-Icon"
+                    style={{ width: 43, height: 43 }}
+                  />
                 </div>
-              </Link>
+                <div>
+                  <h5 className="name">{contact.name}</h5>
+                  <p className="email">{contact.email}</p>
+                </div>
+              </div>
+
               <div className="d-flex align-items-center">
+                <Link
+                  style={{ color: "black", textDecoration: "none" }}
+                  to={`/${contact.id}`}
+                >
+                  <i
+                    className="pi pi-eye me-3"
+                    style={{ fontSize: "1.5em" }}
+                  ></i>
+                </Link>
+
                 <Link to="/edit" state={{ contact: contact }}>
                   {/* <div>
                     <img
@@ -47,6 +53,7 @@ const Contact = ({ contacts, removeContactHandler }) => {
                     style={{ fontSize: "1.5em" }}
                   ></i>
                 </Link>
+
                 <div>
                   {/* <img
                     className="img-fluid me-1"
@@ -62,6 +69,7 @@ const Contact = ({ contacts, removeContactHandler }) => {
                       );
                     }}
                   /> */}
+
                   <i
                     className="pi pi-trash me-1"
                     style={{ fontSize: "1.3em" }}
